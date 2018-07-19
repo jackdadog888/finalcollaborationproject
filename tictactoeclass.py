2526fd7899ddb79a60d99b9828e2af02282fa2dd
N1Sha <41408199+N1Sha@users.noreply.github.com>
2018-07-18 20:14:27 -0700

    Update tictactoeclass.py

--- tictactoeclass.py
+++ tictactoeclass.py
@@ -1,109 +1,107 @@
 import random
 class TicTacToe:
-  def drawBoard(board):
-    # This function prints out the board that it was passed.
+    def drawBoard(board):
+        # This function prints out the board that it was passed.
 
-    # "board" is a list of 10 strings representing the board (ignore index 0)
-    print('   |   |')
-    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
-    print('   |   |')
-    print('-----------')
-    print('   |   |')
-    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
-    print('   |   |')
-    print('-----------')
-    print('   |   |')
-    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
-    print('   |   |')
+        # "board" is a list of 10 strings representing the board (ignore index 0)
+        print('   |   |')
+        print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+        print('   |   |')
+        print('-----------')
+        print('   |   |')
+        print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
+        print('   |   |')
+        print('-----------')
+        print('   |   |')
+        print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
+        print('   |   |')
 
-def inputplayer1Letter():
-    # Lets the player 1 type which letter they want to be.
-    # Returns a list with the player 1's letter as the first item, and the player 2's letter as the second.
-    letter = ''
-    while not (letter == 'X' or letter == 'O'):
-        print('Hey! Player 1, do you want to be X or O?')
-        letter = input().upper()
+    def inputplayer1Letter():
+        # Lets the player 1 type which letter they want to be.
+        # Returns a list with the player 1's letter as the first item, and the player 2's letter as the second.
+        letter = ''
+        while not (letter == 'X' or letter == 'O'):
+            print('Hey! Player 1, do you want to be X or O?')
+            letter = input().upper()
 
-    # the first element in the tuple is the player 1's letter, the second is the player 2's letter.
-    if letter == 'X':
-        return ['X', 'O']
-    else:
-        return ['O', 'X']
+        # the first element in the tuple is the player 1's letter, the second is the player 2's letter.
+        if letter == 'X':
+            return ['X', 'O']
+        else:
+            return ['O', 'X']
 
-def whoGoesFirst():
-    # Randomly choose the player 1 who goes first.
-    if random.randint(0, 1) == 0:
-        return 'Player 2'
-    else:
-        return 'Player 1'
+    def whoGoesFirst():
+        # Randomly choose the player 1 who goes first.
+        if random.randint(0, 1) == 0:
+            return 'Player 2'
+        else:
+            return 'Player 1'
+    def playAgain():
+        # This function returns True if the player 1 wants to play again, otherwise it returns False.
+        print('Do you want to play again? (yes or no)')
+        return input().lower().startswith('y')
 
-def playAgain():
-    # This function returns True if the player 1 wants to play again, otherwise it returns False.
-    print('Do you want to play again? (yes or no)')
-    return input().lower().startswith('y')
+    def makeMove(board, letter, move):
+        board[move] = letter
 
-def makeMove(board, letter, move):
-    board[move] = letter
+    def isWinner(bo, le):
+        # Given a board and a player1's letter, this function returns True if that player1 has won.
+        # We use bo instead of board and le instead of letter so we don't have to type as much.
+        return ((bo[7] == le and bo[8] == le and bo[9] == le) or # across the top
+        (bo[4] == le and bo[5] == le and bo[6] == le) or # across the middle
+        (bo[1] == le and bo[2] == le and bo[3] == le) or # across the bottom
+        (bo[7] == le and bo[4] == le and bo[1] == le) or # down the left side
+        (bo[8] == le and bo[5] == le and bo[2] == le) or # down the middle
+        (bo[9] == le and bo[6] == le and bo[3] == le) or # down the right side
+        (bo[7] == le and bo[5] == le and bo[3] == le) or # diagonal
+        (bo[9] == le and bo[5] == le and bo[1] == le)) # diagonal
 
-def isWinner(bo, le):
-    # Given a board and a player1's letter, this function returns True if that player1 has won.
-    # We use bo instead of board and le instead of letter so we don't have to type as much.
-    return ((bo[7] == le and bo[8] == le and bo[9] == le) or # across the top
-    (bo[4] == le and bo[5] == le and bo[6] == le) or # across the middle
-    (bo[1] == le and bo[2] == le and bo[3] == le) or # across the bottom
-    (bo[7] == le and bo[4] == le and bo[1] == le) or # down the left side
-    (bo[8] == le and bo[5] == le and bo[2] == le) or # down the middle
-    (bo[9] == le and bo[6] == le and bo[3] == le) or # down the right side
-    (bo[7] == le and bo[5] == le and bo[3] == le) or # diagonal
-    (bo[9] == le and bo[5] == le and bo[1] == le)) # diagonal
+    def getBoardCopy(board):
+        # Make a duplicate of the board list and return it the duplicate.
+        dupeBoard = []
 
-def getBoardCopy(board):
-    # Make a duplicate of the board list and return it the duplicate.
-    dupeBoard = []
+        for i in board:
+            dupeBoard.append(i)
 
-    for i in board:
-        dupeBoard.append(i)
+        return dupeBoard
+    def isSpaceFree(board, move):
+        # Return true if the passed move is free on the passed board.
+        return board[move] == ' '
 
-    return dupeBoard
+    def getplayer1Move(board):
+        # Let the player 1 type in his move.
+        copy = getBoardCopy(board)
+        move = ' '
+        while move not in '1 2 3 4 5 6 7 8 9'.split() or not isSpaceFree(board, int(move)):
+            print('PLayer 1, what is your next move? (1-9)')
+            move = input()
+        return int(move)
 
-def isSpaceFree(board, move):
-    # Return true if the passed move is free on the passed board.
-    return board[move] == ' '
+    def getplayer2Move(board):
+        # Let the player 2 type in his move.
+        copy = getBoardCopy(board)
+        move = ' '
+        while move not in '1 2 3 4 5 6 7 8 9'.split() or not isSpaceFree(board, int(move)):
+            print('Player 2, what is your next move? (1-9)')
+            move = input()
+        return int(move)
 
-def getplayer1Move(board):
-    # Let the player 1 type in his move.
-    copy = getBoardCopy(board)
-    move = ' '
-    while move not in '1 2 3 4 5 6 7 8 9'.split() or not isSpaceFree(board, int(move)):
-        print('PLayer 1, what is your next move? (1-9)')
-        move = input()
-    return int(move)
+    def chooseRandomMoveFromList(board, movesList):
+        # Returns a valid move from the passed list on the passed board.
+        # Returns None if there is no valid move.
+        possibleMoves = []
+        for i in movesList:
+            if isSpaceFree(board, i):
+                possibleMoves.append(i)
 
-def getplayer2Move(board):
-    # Let the player 2 type in his move.
-    copy = getBoardCopy(board)
-    move = ' '
-    while move not in '1 2 3 4 5 6 7 8 9'.split() or not isSpaceFree(board, int(move)):
-        print('Player 2, what is your next move? (1-9)')
-        move = input()
-    return int(move)
+        if len(possibleMoves) != 0:
+            return random.choice(possibleMoves)
+        else:
+            return None
 
-def chooseRandomMoveFromList(board, movesList):
-    # Returns a valid move from the passed list on the passed board.
-    # Returns None if there is no valid move.
-    possibleMoves = []
-    for i in movesList:
-        if isSpaceFree(board, i):
-            possibleMoves.append(i)
-
-    if len(possibleMoves) != 0:
-        return random.choice(possibleMoves)
-    else:
-        return None
-
-def isBoardFull(board):
-    # Return True if every space on the board has been taken. Otherwise return False.
-    for i in range(1, 10):
-        if isSpaceFree(board, i):
-            return False
-    return True
+    def isBoardFull(board):
+        # Return True if every space on the board has been taken. Otherwise return False.
+        for i in range(1, 10):
+            if isSpaceFree(board, i):
+                return False
+        return True
